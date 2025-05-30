c00fb713d77a1b9936e016af43e5f22009cd27cf
Darek <darecki9k@gmail.com>
2025-05-30 13:08:06 +0200

    feat(AppProviders): add AppProviders component with Redux and React Router integration

---
+++ frontend/src/components/providers/AppProviders.tsx
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from '@store/index';
+import { ErrorBoundary } from '@components/molecules/ErrorBoundary/ErrorBoundary';
+
+interface AppProvidersProps {
+    children: React.ReactNode;
+}
+
+export const AppProviders = memo<AppProvidersProps>(({ children }) => {
+    return (
+        <ErrorBoundary>
+            <Provider store={store}>
+                <BrowserRouter>
+                    {children}
+                </BrowserRouter>
+            </Provider>
+        </ErrorBoundary>
+    );
+});
+
+AppProviders.displayName = 'AppProviders';
