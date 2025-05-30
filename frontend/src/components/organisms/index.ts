cac17780f2547881e8ff7156ec40415381b36e71
Darek <darecki9k@gmail.com>
2025-05-30 13:07:50 +0200

    feat(index): export TestResultsTable and ChartContainer components

---
+++ frontend/src/components/organisms/index.ts
@@ -0,0 +1,9 @@
+export {
+  TestResultsTable,
+  type TestResultsTableProps,
+  type TestResultRow,
+} from "./TestResultsTable/TestResultsTable";
+export {
+  ChartContainer,
+  type ChartContainerProps,
+} from "./ChartContainer/ChartContainer";
