fe77af965efe5e2dbfa4097d408171ff8ce6b3a8
Darek <darecki9k@gmail.com>
2025-05-30 13:07:00 +0200

    feat(molecules): add index file to export MetricCard, SearchBox, and TestSelector components

---
+++ frontend/src/components/molecules/index.ts
@@ -0,0 +1,7 @@
+export { MetricCard, type MetricCardProps } from "./MetricCard/MetricCard";
+export { SearchBox, type SearchBoxProps } from "./SearchBox/SearchBox";
+export {
+  TestSelector,
+  type TestSelectorProps,
+  type TestOption,
+} from "./TestSelector/TestSelector";
