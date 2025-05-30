340bc10392ced84545236ca0aafe0a0606d9d840
Darek <darecki9k@gmail.com>
2025-05-30 13:06:22 +0200

    feat(Badge): add Badge component with styling and props

---
+++ frontend/src/components/atoms/index.ts
@@ -0,0 +1,5 @@
+export { Button, type ButtonProps } from "./Button/Button";
+export { Input, type InputProps } from "./Input/Input";
+export { Spinner, type SpinnerProps } from "./Spinner/Spinner";
+export { Badge, type BadgeProps } from "./Badge/Badge";
+export { Icon, type IconProps } from "./Icon/Icon";
