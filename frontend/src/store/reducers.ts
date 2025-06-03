d8375fe8874296cc33fb41de715a0bd1b49f2d49
Darek <darecki9k@gmail.com>
2025-06-03 06:51:45 +0200

    refactor: remove reducers and repositories saga for cleanup

--- frontend/src/store/reducers.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import testResultsReducer from "./slices/testResultsSlice";
-import repositoriesReducer from "./slices/repositoriesSlice";
-
-export const rootReducer = combineReducers({
-  testResults: testResultsReducer,
-  repositories: repositoriesReducer,
-});
--- frontend/src/store/sagas/index.ts
+++ frontend/src/store/sagas/index.ts
@@ -1,7 +1,15 @@
-import { all } from "redux-saga/effects";
-import { testResultsSaga } from "./testResultsSaga";
-import { repositoriesSaga } from "./repositoriesSaga";
+
+import { all, fork } from 'redux-saga/effects';
+import testSaga from './testSaga';
+import dashboardSaga from './dashboardSaga';
+import terminalSaga from './terminalSaga';
+import repositorySaga from './repositorySaga';
 
 export default function* rootSaga() {
-  yield all([testResultsSaga(), repositoriesSaga()]);
+  yield all([
+    fork(testSaga),
+    fork(dashboardSaga),
+    fork(terminalSaga),
+    fork(repositorySaga),
+  ]);
 }
